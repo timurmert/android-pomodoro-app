fa89b380b54cd4d196c0b7e808f14af65b40f1f2
timurmert <90184236+timurmert@users.noreply.github.com>
2025-11-02 15:28:24 +0300

    Use Java 17 toolchain for domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -5,12 +5,17 @@ plugins {
 
 java {
     toolchain {
+        // Align with the project's Java 17 requirement so that Gradle can reuse a
+        // locally installed JDK without downloading Java 21.
         languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 kotlin {
     jvmToolchain(17)
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
